24acc2346077283ea54d5f9b2f0e793776d5d0ec
Jéssica Pereira <jessicapereiraa2@gmail.com>
2021-09-18 11:32:45 -0300

    configuração firebase

--- Se_cuida_ai/lib/main.dart
+++ Se_cuida_ai/lib/main.dart
@@ -1,20 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'login.dart';
 
-void main() => runApp(
-  MaterialApp(
-    localizationsDelegates: [
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate
-    ],
-    supportedLocales: [const Locale('pt', 'BR')],
-    home: login(),
-    theme: ThemeData(
-      primaryColor: Colors.black,
-        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black)
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
-    ),
-    debugShowCheckedModeBanner: false,
-  )
-);
+
+  runApp(
+      MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate
+        ],
+        supportedLocales: [const Locale('pt', 'BR')],
+        home: login(),
+        theme: ThemeData(
+            primaryColor: Colors.black,
+            colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black)
+
+        ),
+        debugShowCheckedModeBanner: false,
+      )
+  );
+}
