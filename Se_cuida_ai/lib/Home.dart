dfb63f4e5a8ae96c9118176a6c410a8ff10e9475
Jéssica <jessicapereiraa2@gmail.com>
2021-09-25 14:47:56 -0300

    pag inicial paciente

--- Se_cuida_ai/lib/Home.dart
+++ Se_cuida_ai/lib/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class telaInicial extends StatefulWidget {
 
@@ -8,8 +10,70 @@ class telaInicial extends StatefulWidget {
 }
 
 class _telaInicialState extends State<telaInicial> {
+
+  List profissoes = ['Fisioterapia', 'Enfermagem','Fisioterapia', 'Enfermagem', 'Médico', 'Dentista', 'Massagista', 'Técnico de enfermagem'];
+  List cores = ['#D6967B', '#FF1D59','#845EC2', '#D65DB1', '#FF6F91', '#FF9671', '#FFC75F', 'F9F871'];
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar:AppBar(
+        backgroundColor: Colors.grey,
+        title: Text("Se cuida aí"),
+      ),
+      body: _viewList()
+    );
   }
+  Widget _viewList() => GridView.builder(
+      padding: EdgeInsets.all(16),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+      mainAxisSpacing: 12,
+      crossAxisSpacing: 12,
+      childAspectRatio: 1),
+      itemCount: profissoes.length,
+      itemBuilder: (context, index){
+        final item = profissoes[index];
+        return _cardprofissao(item, index);
+      });
+
+  Widget _cardprofissao(String prof,int index) => GestureDetector(
+    onTap: (){
+      print(profissoes[index].toString());
+    },
+    child: Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          color: HexColor(cores[index]),
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.6),
+                blurRadius: 8,
+                spreadRadius: 5,
+                offset: Offset(
+                    0,3
+                )
+            )
+          ]
+
+      ),
+      padding: EdgeInsets.all(16),
+
+      child: GridTile(
+        child: Center(
+          child: Text(
+            prof,
+            style: TextStyle(fontWeight:  FontWeight.bold, fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    ),
+  );
 }
+
+
